3aa06736c6a331fc26de5991f3a71ebeaa563d4c
EllizzInBits <elizabethm174394@gmail.com>
2025-07-10 13:10:10 -0300

    Update start-all.js

--- start-all.js
+++ start-all.js
@@ -1,18 +1,22 @@
 const { spawn } = require('child_process');
 
 function run(name, command, args, cwd) {
-  const proc = spawn(command, args, { cwd, stdio: 'inherit', shell: true });
+  const proc = spawn(command, args, {
+    cwd,
+    shell: true,
+    stdio: 'inherit',
+  });
   proc.on('exit', (code) => {
     if (code !== 0) {
-      console.error(`${name} saiu com código ${code}`);
+      console.error(`${name} exited with code ${code}`);
       process.exit(code);
     }
   });
   return proc;
 }
 
-const proc1 = run('sistema1', 'npm', ['start'], './SistemaPrincipal');
-const proc2 = run('sistema2', 'npm', ['run', 'dev'], './wppconnect-server');
+const proc1 = run('SistemaPrincipal', 'npm', ['start'], 'SistemaPrincipal');
+const proc2 = run('wppconnect-server', 'npm', ['run', 'dev'], 'wppconnect-server');
 
 process.on('SIGINT', () => {
   proc1.kill('SIGINT');
