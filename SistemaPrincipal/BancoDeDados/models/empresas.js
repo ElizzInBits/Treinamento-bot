17f9e73ac5d74dea7bcf6aad6d30ef078ca085e1
EllizzInBits <elizabethm174394@gmail.com>
2025-07-15 13:41:33 -0300

    add model/route

---
+++ SistemaPrincipal/BancoDeDados/models/empresas.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database');
+
+const Empresa = sequelize.define('empresas', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  razao_social: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cnpj: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  porte_empresa: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  endereco: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cep: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  contato: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  criado_em: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  timestamps: false,           
+  freezeTableName: true        
+});
+
+module.exports = Empresa;
