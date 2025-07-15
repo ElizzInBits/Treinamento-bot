f250b72b7b2cf066500f68264b76802dc3a19ab5
EllizzInBits <elizabethm174394@gmail.com>
2025-07-15 08:26:34 -0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     {
       name: "front-end-cadastro",
-      cwd: "./SistemaPrincipal/front-end-cadastro",
+      cwd: "./SistemaPrincipal/front-end",
       script: "npm",
       args: " run start",
       interpreter: "none"
