105f418e08a94bc2a9a672ac206e16582c6f9374
EllizzInBits <elizabethm174394@gmail.com>
2025-07-17 13:36:07 -0300

    ajuste

--- SistemaPrincipal/front-end/routes/empresas.js
+++ SistemaPrincipal/front-end/routes/empresas.js
@@ -1,7 +1,7 @@
 const { Op } = require('sequelize');
 const express = require('express');
 const router = express.Router();
-const { Empresa } = require('../../BancoDeDados/models/empresas');
+const { Empresa } = require('../../BancoDeDados/models');
 
 function limparCNPJ(cnpj) {
   return cnpj.replace(/\D/g, '');
