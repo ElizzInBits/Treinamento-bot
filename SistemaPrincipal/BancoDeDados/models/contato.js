394d76524dc9eb57bcbbd58b608e73a36d0cdb7d
EllizzInBits <elizabethm174394@gmail.com>
2025-07-17 13:01:51 -0300

    update

--- SistemaPrincipal/BancoDeDados/models/contato.js
+++ SistemaPrincipal/BancoDeDados/models/contato.js
@@ -1,58 +1,60 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../database');
-const Empresa = require('./empresas')
-const Contato = sequelize.define('contatos', {
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  telefone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  nomeCompleto: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  cpf: {
-    type: DataTypes.CHAR(11),
-    allowNull: true,
-    unique: true,
-  },
-  empresa: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  statusTreinamento: {
-    type: DataTypes.STRING,
-    defaultValue: 'não iniciado',
-  },
-  treinamentoId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
-  empresaId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: 'empresas',
-      key: 'id'
-    }
-  }
-}, {
-  timestamps: false,
-  freezeTableName: true,
-});
 
-Contato.belongsTo(Empresa, { foreignKey: 'empresaId' });
-module.exports = Contato;
+module.exports = (sequelize) => {
+  const Contato = sequelize.define('contatos', {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telefone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    nomeCompleto: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    cpf: {
+      type: DataTypes.CHAR(11),
+      allowNull: true,
+      unique: true,
+    },
+    empresa: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    statusTreinamento: {
+      type: DataTypes.STRING,
+      defaultValue: 'não iniciado',
+    },
+    treinamentoId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    empresaId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'empresas',
+        key: 'id',
+      },
+    },
+  }, {
+    timestamps: false,
+    freezeTableName: true,
+  });
 
+  Contato.associate = (models) => {
+    Contato.belongsTo(models.Empresa, { foreignKey: 'empresaId' });
+  };
 
+  return Contato;
+};
