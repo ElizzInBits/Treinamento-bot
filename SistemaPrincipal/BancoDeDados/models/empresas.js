4b1a7ef181ec225796192e505e4e4e94f50c5782
EllizzInBits <elizabethm174394@gmail.com>
2025-07-17 13:01:51 -0300

    update

--- SistemaPrincipal/BancoDeDados/models/empresas.js
+++ SistemaPrincipal/BancoDeDados/models/empresas.js
@@ -1,53 +1,57 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../database'); 
-const Contato = require('./contato');
-const Empresa = sequelize.define('empresas', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  razao_social: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  cnpj: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  porte_empresa: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  endereco: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  cep: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  contato: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  email: {                     
-    type: DataTypes.STRING,
-    allowNull: true,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  criado_em: {
-    type: DataTypes.DATE,
-    allowNull: true
-  }
-}, {
-  timestamps: false,
-  freezeTableName: true
-});
 
-Empresa.hasMany(Contato, { foreignKey: 'empresaId' });
-module.exports = Empresa;
+module.exports = (sequelize) => {
+  const Empresa = sequelize.define('empresas', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    razao_social: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cnpj: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    porte_empresa: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    endereco: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cep: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    contato: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    criado_em: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  }, {
+    timestamps: false,
+    freezeTableName: true,
+  });
+
+  Empresa.associate = (models) => {
+    Empresa.hasMany(models.Contato, { foreignKey: 'empresaId' });
+  };
+
+  return Empresa;
+};
