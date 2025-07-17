a3c31c5597b88a1c698d8de76650f81b665e6f4b
EllizzInBits <elizabethm174394@gmail.com>
2025-07-17 13:32:56 -0300

    ajuste

--- SistemaPrincipal/routes/empresas.js
+++ SistemaPrincipal/routes/empresas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Empresa = require('../BancoDeDados/models/empresas');
+const Empresa = require('../../BancoDeDados/models');
 const { Op } = require('sequelize');
 
 const { cnpj: cnpjValidator } = require('cpf-cnpj-validator');
