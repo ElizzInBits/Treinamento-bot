3403bac43e2f850fca5923e4e2addc28ae5a61af
EllizzInBits <elizabethm174394@gmail.com>
2025-07-29 11:40:27 -0300

    Update certificados2.js

--- SistemaPrincipal/TemplatesMensagens/Certificados/certificados2.js
+++ SistemaPrincipal/TemplatesMensagens/Certificados/certificados2.js
@@ -36,8 +36,8 @@ async function gerarCertificadoBanco(contatoId) {
     const tamanho = 11;
 
     // Nome da pessoa (centralizado)
-    page.drawText('Conferido a:', { x: 270, y: 560, size: tamanho, font: helvetica, color: cor });
-    page.drawText(contato.nomeCompleto || contato.nome, { x: 200, y: 540, size: tamanho + 2, font: helvetica, color: cor });
+    page.drawText('Conferido a:', { x: 270, y: 590, size: tamanho, font: helvetica, color: cor });
+    page.drawText(contato.nomeCompleto || contato.nome, { x: 200, y: 575, size: tamanho + 2, font: helvetica, color: cor });
 
     // LAYOUT EM FORMATO DE TABELA - IGUAL AO PDF 2
 
