5ad9c4ef90023525f3b8bd427f2bbae7329f6411
EllizzInBits <elizabethm174394@gmail.com>
2025-08-01 11:55:35 -0300

    Update certificados2.js

--- SistemaPrincipal/TemplatesMensagens/Certificados/certificados2.js
+++ SistemaPrincipal/TemplatesMensagens/Certificados/certificados2.js
@@ -228,7 +228,7 @@ async function enviarEmail(destinatario, arquivoPath) {
       throw new Error('❌ Arquivo de certificado não encontrado.');
     }
 
-    const transporter = nodemailer.createTransporter({
+    const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER || 'seu-email@gmail.com',
