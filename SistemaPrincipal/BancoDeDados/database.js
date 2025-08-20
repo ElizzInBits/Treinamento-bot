6e62178ebf22e8fc427d7a439c78d965f66c9e4c
root <root@srv966001.hstgr.cloud>
2025-08-20 13:45:36 +0000

    Bora lá

--- SistemaPrincipal/BancoDeDados/database.js
+++ SistemaPrincipal/BancoDeDados/database.js
@@ -5,12 +5,21 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'listadecontatos',
   process.env.DB_USER || 'root', 
-  process.env.DB_PASS || 'admin!?',
+  process.env.DB_PASS || 'Admin!?',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+	connectionTimeout: 60000,
+    },
+    pool: {
+	max: 5,
+	min: 0,
+	acquire: 30000,
+	idle: 10000
+    }
   }
 );
 
