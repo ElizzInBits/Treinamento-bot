75835f580129d13c7bb2414478afd98b0cebc0e5
EllizzInBits <elizabethm174394@gmail.com>
2025-08-26 08:35:03 -0300

    Update auth.ts

--- wppconnect-server/src/middleware/auth.ts
+++ wppconnect-server/src/middleware/auth.ts
@@ -23,70 +23,14 @@ function formatSession(session: string) {
 }
 
 const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
-  const secureToken = req.serverOptions.secretKey;
-
   const { session } = req.params;
-  const { authorization: token } = req.headers;
   if (!session)
     return res.status(401).send({ message: 'Session not informed' });
 
-  try {
-    let tokenDecrypt = '';
-    let sessionDecrypt = '';
-
-    try {
-      sessionDecrypt = session.split(':')[0];
-      tokenDecrypt = session
-        .split(':')[1]
-        .replace(/_/g, '/')
-        .replace(/-/g, '+');
-    } catch (error) {
-      try {
-        if (token && token !== '' && token.split(' ').length > 0) {
-          const token_value = token.split(' ')[1];
-          if (token_value)
-            tokenDecrypt = token_value.replace(/_/g, '/').replace(/-/g, '+');
-          else
-            return res.status(401).json({
-              message: 'Token is not present. Check your header and try again',
-            });
-        } else {
-          return res.status(401).json({
-            message: 'Token is not present. Check your header and try again',
-          });
-        }
-      } catch (e) {
-        req.logger.error(e);
-        return res.status(401).json({
-          error: 'Check that a Session and Token are correct',
-          message: error,
-        });
-      }
-    }
-
-    bcrypt.compare(
-      sessionDecrypt + secureToken,
-      tokenDecrypt,
-      function (err, result) {
-        if (result) {
-          req.session = formatSession(req.params.session);
-          req.token = tokenDecrypt;
-          req.client = clientsArray[req.session];
-          next();
-        } else {
-          return res
-            .status(401)
-            .json({ error: 'Check that the Session and Token are correct' });
-        }
-      }
-    );
-  } catch (error) {
-    req.logger.error(error);
-    return res.status(401).json({
-      error: 'Check that the Session and Token are correct.',
-      message: error,
-    });
-  }
+  // Bypass authentication for testing
+  req.session = formatSession(session);
+  req.client = clientsArray[req.session];
+  next();
 };
 
 export default verifyToken;
