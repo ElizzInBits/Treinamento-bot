2f1c1c57a2435912430356fa719f7652d35bcb92
EllizzInBits <elizabethm174394@gmail.com>
2025-09-29 12:37:22 -0300

    Update gerarCertificado.js

--- SistemaPrincipal/TemplatesMensagens/Certificados/gerarCertificado.js
+++ SistemaPrincipal/TemplatesMensagens/Certificados/gerarCertificado.js
@@ -34,7 +34,7 @@ async function gerarCertificado(nome, email, sendMessage = null, sender = null) 
     
     page.drawText(nomeCompleto, { 
       x: nomeX, 
-      y: 800, 
+      y: 300, 
       size: nomeSize, 
       font: helvetica, 
       color: rgb(0, 0, 0) 
