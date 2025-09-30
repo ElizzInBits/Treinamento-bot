6a81886f84caabffdc212cf25b5151e1d5e59d92
EllizzInBits <elizabethm174394@gmail.com>
2025-09-30 11:08:31 -0300

    Update treinamentoApresentacao.js

--- SistemaPrincipal/TemplatesMensagens/Treinamentos/Apresentacao/treinamentoApresentacao.js
+++ SistemaPrincipal/TemplatesMensagens/Treinamentos/Apresentacao/treinamentoApresentacao.js
@@ -59,6 +59,9 @@ async function processarEstadoAtual(sender, text, selectedId, contato, ultimaInt
         case 'finalizando':
             console.log('🔄 Ignorando mensagem - finalização já em andamento');
             return true;
+        case 'processando_cadastro':
+            console.log('🔄 Ignorando mensagem - cadastro já sendo processado');
+            return true;
         default:
             return await iniciarFluxoBoasVindas(sender, sendMessage);
     }
@@ -88,6 +91,9 @@ async function processarOpcaoInicial(sender, text, sendMessage, buscarContato = 
         }
         
         if (!contato) {
+            // Marcar como processando cadastro para evitar duplicação
+            await salvarInteracao(sender, 'processando_cadastro', JSON.stringify({ etapa: 'processando_cadastro' }));
+            
             setTimeout(async () => {
                 await sendMessage(sender, 'send-message', {
                     message: '🤔 Hum, que tal fazer o seu cadastro na nossa plataforma antes, hein?\nÉ muito simples, basta clicar no link abaixo e assim que finalizar é só voltar aqui e me envie qualquer mensagem para começarmos!\n\nhttps://abrir.link/ZEeCt\n\nATENÇÃO:\nNo Cadastro use o MESMO NÚMERO que você utilizará para conversar aqui comigo.\n\n💡 Caso tenha feito cadastro com um número diferente desse, basta acessar novamente o painel de cadastro, rolar a tela até o final e acessar os seus dados para realizar a edição do número.'
